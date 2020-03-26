b368d20ac0979caac080ebf0e320f7bb6285bb8b
RivaldiHariono <40395498+RivaldiHariono@users.noreply.github.com>
2020-03-26 20:29:45 +0700

    Final Commit

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let tombol = document.querySelector(".container-tombol");
 let layar = document.querySelector("#layar");
 let clear = document.querySelector(".clear");
 let del = document.querySelector(".del");
+let temp = '';
+let operator = '';
+let count = false;
+let reset = false;
 
 
 tombol.addEventListener("click",function(e) {
@@ -9,12 +13,38 @@ tombol.addEventListener("click",function(e) {
     let num = btnClick.innerText;
 
     if (num == '=') {
-        layar.value = eval(layar.value)
+        layar.value = eval(temp + operator + layar.value)
     }
 
-    else if(num != 'C' && num != 'Del'){
-        layar.value = layar.value + num;
+    else if(num == 'C'){
+        layar.value = "";
     }
+
+    else if(num == 'Del'){
+        layar.value = layar.value.slice(0,-1);
+    }
+
+    else if(btnClick.classList.contains('operator')){
+        if(count == true){
+            layar.value = eval(temp + operator + layar.value)
+            count = false;
+        }
+        temp = layar.value;
+        operator = num; 
+        reset = true;
+    }
+
+    else{
+        if(reset == true){
+            layar.value = num;
+            reset = false;
+            count = true;
+        }
+        else{
+            layar.value = layar.value + num;
+        }
+    }
+
 });
 
 clear.addEventListener("click",function(e){
